799c53f4d9caf9a539d8a555f243e6fd19a8a11b
G4llardoUAM <115124478+G4llardoUAM@users.noreply.github.com>
2022-12-17 17:53:14 -0600

    Actaulizacion de la información

--- negocios/text.cs
+++ negocios/text.cs
@@ -3,12 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Dynamic;
 
 namespace negocios
 {
     internal class text
     {
 
+        public void archivo_guardar()
+        {
+
+
+            /*Crear botno donde se va a meter este codigo */
+
+            StreamWriter escribir = new StreamWriter(@"C:\\Users\\Andrey\\Desktop\archivo_texto.txt", true);
+            try
+            {
+
+                escribir.WriteLine("Nombre del label donde esta la informacion box" + /*Nombre del text box  --- sentencia = +textBox.Text*/);
+                escribir.WriteLine("Nombre del label donde esta la informacion box" + /*Nombre del text box*/);
+                escribir.WriteLine("Nombre del label donde esta la informacion box" + /*Nombre del text box*/);
+                escribir.WriteLine("Nombre del label donde esta la informacion box" + /*Nombre del text box*/);
+                escribir.WriteLine("Nombre del label donde esta la informacion box" + /*Nombre del text box*/);
+                escribir.WriteLine("\n");
+
+                
+
+            }
+
+            catch
+            {
+                
+            }
+
+        }
+
+
+
+
 
     }
-}
+
+ }
+     
+
